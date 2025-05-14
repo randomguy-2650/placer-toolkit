d5f0147e9890d6129d7f68a9b117533f0d424140
Sy Dan Phan <150704902+randomguy-2650@users.noreply.github.com>
2025-05-14 17:18:16 +0200

    Finish Checkbox story and add Icon story

--- src/components/checkbox/Checkbox.stories.ts
+++ src/components/checkbox/Checkbox.stories.ts
@@ -12,18 +12,31 @@ const meta: Meta = {
         hint: { control: "text" },
         indeterminate: { control: "boolean" },
         invalid: { control: "boolean" },
+        label: { control: "text" },
         name: { control: "text" },
         pressed: { control: "boolean" },
+        required: { control: "boolean" },
+        size: {
+            control: "select",
+            options: ["small", "medium", "large"],
+        },
+        title: { control: "text" },
+        value: { control: "text" },
     },
     args: {
         checked: false,
         defaultChecked: false,
         disabled: false,
-        hint: undefined,
+        hint: "",
         indeterminate: false,
         invalid: false,
-        name: undefined,
+        label: "Checkbox",
+        name: "",
         pressed: false,
+        required: false,
+        size: "medium",
+        title: "",
+        value: "",
     },
 };
 
@@ -39,18 +52,335 @@ export const Default: Story = {
         hint,
         indeterminate,
         invalid,
+        label,
         name,
         pressed,
+        required,
+        size,
+        title,
+        value,
     }) => html`
         <pc-checkbox
-            ?checked="${checked}"
-            ?defaultChecked="${defaultChecked}"
-            ?disabled="${disabled}"
-            hint="${hint}"
-            ?indeterminate="${indeterminate}"
-            ?invalid="${invalid}"
-            name="${name}"
-            pressed="${pressed}"
-        ></pc-checkbox>
+            ?checked=${checked}
+            ?defaultChecked=${defaultChecked}
+            ?disabled=${disabled}
+            hint=${hint}
+            ?indeterminate=${indeterminate}
+            ?invalid=${invalid}
+            name=${name}
+            ?pressed=${pressed}
+            ?required=${required}
+            size=${size}
+            title=${title}
+            value=${value}
+        >
+            ${label}
+        </pc-checkbox>
+    `,
+};
+
+export const Checked: Story = {
+    args: {
+        checked: true,
+        defaultChecked: false,
+        disabled: false,
+        hint: "",
+        indeterminate: false,
+        invalid: false,
+        label: "Checked",
+        name: "",
+        pressed: false,
+        required: false,
+        size: "medium",
+        title: "",
+        value: "",
+    },
+
+    render: ({
+        checked,
+        defaultChecked,
+        disabled,
+        hint,
+        indeterminate,
+        invalid,
+        label,
+        name,
+        pressed,
+        required,
+        size,
+        title,
+        value,
+    }) => html`
+        <pc-checkbox
+            ?checked=${checked}
+            ?defaultChecked=${defaultChecked}
+            ?disabled=${disabled}
+            hint=${hint}
+            ?indeterminate=${indeterminate}
+            ?invalid=${invalid}
+            name=${name}
+            ?pressed=${pressed}
+            ?required=${required}
+            size=${size}
+            title=${title}
+            value=${value}
+        >
+            ${label}
+        </pc-checkbox>
+    `,
+};
+
+export const Indeterminate: Story = {
+    args: {
+        checked: false,
+        defaultChecked: false,
+        disabled: false,
+        hint: "",
+        indeterminate: true,
+        invalid: false,
+        label: "Indeterminate",
+        name: "",
+        pressed: false,
+        required: false,
+        size: "medium",
+        title: "",
+        value: "",
+    },
+
+    render: ({
+        checked,
+        defaultChecked,
+        disabled,
+        hint,
+        indeterminate,
+        invalid,
+        label,
+        name,
+        pressed,
+        required,
+        size,
+        title,
+        value,
+    }) => html`
+        <pc-checkbox
+            ?checked=${checked}
+            ?defaultChecked=${defaultChecked}
+            ?disabled=${disabled}
+            hint=${hint}
+            ?indeterminate=${indeterminate}
+            ?invalid=${invalid}
+            name=${name}
+            ?pressed=${pressed}
+            ?required=${required}
+            size=${size}
+            title=${title}
+            value=${value}
+        >
+            ${label}
+        </pc-checkbox>
+    `,
+};
+
+export const Hint: Story = {
+    args: {
+        checked: false,
+        defaultChecked: false,
+        disabled: false,
+        hint: "What does this checkbox do?",
+        indeterminate: false,
+        invalid: false,
+        label: "Checkbox with hint",
+        name: "",
+        pressed: false,
+        required: false,
+        size: "medium",
+        title: "",
+        value: "",
+    },
+
+    render: ({
+        checked,
+        defaultChecked,
+        disabled,
+        hint,
+        indeterminate,
+        invalid,
+        label,
+        name,
+        pressed,
+        required,
+        size,
+        title,
+        value,
+    }) => html`
+        <pc-checkbox
+            ?checked=${checked}
+            ?defaultChecked=${defaultChecked}
+            ?disabled=${disabled}
+            hint=${hint}
+            ?indeterminate=${indeterminate}
+            ?invalid=${invalid}
+            name=${name}
+            ?pressed=${pressed}
+            ?required=${required}
+            size=${size}
+            title=${title}
+            value=${value}
+        >
+            ${label}
+        </pc-checkbox>
+    `,
+};
+
+export const Disabled: Story = {
+    args: {
+        checked: false,
+        defaultChecked: false,
+        disabled: true,
+        hint: "",
+        indeterminate: false,
+        invalid: false,
+        label: "Disabled",
+        name: "",
+        pressed: false,
+        required: false,
+        size: "medium",
+        title: "",
+        value: "",
+    },
+
+    render: ({
+        checked,
+        defaultChecked,
+        disabled,
+        hint,
+        indeterminate,
+        invalid,
+        label,
+        name,
+        pressed,
+        required,
+        size,
+        title,
+        value,
+    }) => html`
+        <pc-checkbox
+            ?checked=${checked}
+            ?defaultChecked=${defaultChecked}
+            ?disabled=${disabled}
+            hint=${hint}
+            ?indeterminate=${indeterminate}
+            ?invalid=${invalid}
+            name=${name}
+            ?pressed=${pressed}
+            ?required=${required}
+            size=${size}
+            title=${title}
+            value=${value}
+        >
+            ${label}
+        </pc-checkbox>
+    `,
+};
+
+export const Large: Story = {
+    args: {
+        checked: false,
+        defaultChecked: false,
+        disabled: false,
+        hint: "",
+        indeterminate: false,
+        invalid: false,
+        label: "Large",
+        name: "",
+        pressed: false,
+        required: false,
+        size: "large",
+        title: "",
+        value: "",
+    },
+
+    render: ({
+        checked,
+        defaultChecked,
+        disabled,
+        hint,
+        indeterminate,
+        invalid,
+        label,
+        name,
+        pressed,
+        required,
+        size,
+        title,
+        value,
+    }) => html`
+        <pc-checkbox
+            ?checked=${checked}
+            ?defaultChecked=${defaultChecked}
+            ?disabled=${disabled}
+            hint=${hint}
+            ?indeterminate=${indeterminate}
+            ?invalid=${invalid}
+            name=${name}
+            ?pressed=${pressed}
+            ?required=${required}
+            size=${size}
+            title=${title}
+            value=${value}
+        >
+            ${label}
+        </pc-checkbox>
+    `,
+};
+
+export const Small: Story = {
+    args: {
+        checked: false,
+        defaultChecked: false,
+        disabled: false,
+        hint: "",
+        indeterminate: false,
+        invalid: false,
+        label: "Small",
+        name: "",
+        pressed: false,
+        required: false,
+        size: "small",
+        title: "",
+        value: "",
+    },
+
+    render: ({
+        checked,
+        defaultChecked,
+        disabled,
+        hint,
+        indeterminate,
+        invalid,
+        label,
+        name,
+        pressed,
+        required,
+        size,
+        title,
+        value,
+    }) => html`
+        <pc-checkbox
+            ?checked=${checked}
+            ?defaultChecked=${defaultChecked}
+            ?disabled=${disabled}
+            hint=${hint}
+            ?indeterminate=${indeterminate}
+            ?invalid=${invalid}
+            name=${name}
+            ?pressed=${pressed}
+            ?required=${required}
+            size=${size}
+            title=${title}
+            value=${value}
+        >
+            ${label}
+        </pc-checkbox>
     `,
 };
